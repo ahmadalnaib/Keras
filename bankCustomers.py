8e9ccfc8e2641491f7f3186a8c6d93cd54a9f521
ahmadalnaib <alnaib888@gmail.com>
2026-04-22 12:05:35 +0200

    Add initial implementation for customer churn analysis with data visualization and oversampling

---
+++ bankCustomers.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+from imblearn.over_sampling import RandomOverSampler 
+
+df=pd.read_csv('Churn_Modelling.csv')
+print(df.head())
+
+value_counts=df['Exited'].value_counts()
+print(value_counts)
+
+labels=value_counts.index
+counts=value_counts.values
+
+plt.pie(counts, labels=labels, autopct='%1.1f%%', startangle=90)
+plt.title('Distribution of Exited Customers')
+# plt.show()
+
+input_columns=df.drop('Exited', axis=1)
+target_column=df['Exited']
+
+oversampler=RandomOverSampler(random_state=0)
+input_columns_resampled, target_column_resampled=oversampler.fit_resample(input_columns, target_column)
+df_balanced=pd.concat([input_columns_resampled, target_column_resampled], axis=1)
+class_distribution=df_balanced['Exited'].value_counts()
+print(class_distribution)
