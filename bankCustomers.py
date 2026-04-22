a6e9ac9d9a7172eae13176a379ea7f480eb5d304
ahmadalnaib <alnaib888@gmail.com>
2026-04-22 13:40:13 +0200

    Refactor data preprocessing steps for customer churn analysis, including one-hot encoding and standard scaling

--- bankCustomers.py
+++ bankCustomers.py
@@ -2,6 +2,12 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from imblearn.over_sampling import RandomOverSampler 
 from sklearn.preprocessing import LabelEncoder
+import numpy as np
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler
+
 
 df=pd.read_csv('Churn_Modelling.csv')
 print(df.head())
@@ -34,3 +40,21 @@ print(y)
 labelencoder_gender=LabelEncoder()
 x[:, 2]=labelencoder_gender.fit_transform(x[:, 2])
 print(x[:,2])
+
+labelencoder_gender.transform(['Female', 'Male'])
+# array([0, 1])
+
+distinct_values=np.unique(x[:, 1])
+print(distinct_values)
+column_transformer=ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [1])], remainder='passthrough')
+x=column_transformer.fit_transform(x)
+print(x)
+
+x_train, x_test, y_train, y_test=train_test_split(x, y, test_size=0.2, random_state=0)
+
+
+sc=StandardScaler()
+x_train=sc.fit_transform(x_train)
+x_test=sc.transform(x_test)
+print(x_train)
+
